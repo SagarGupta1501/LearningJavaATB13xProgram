9c0b67e3719a2e01e1b122e23093b9208ae785d9
SagarGupta1501 <sagargup15@gmail.com>
2025-07-05 16:57:18 +0530

    Test2

---
+++ src/ex_01_Java_Basics/Test2.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basics;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int d = a+b;
+        int c = a-b;
+
+        int m = a*b;
+
+        float f = a/b;
+
+        int mod = a%b;
+
+        System.out.println("Addition: " + d);
+        System.out.println("Subtraction: " + c);
+        System.out.println("Multiplication: " + m);
+        System.out.println("Division: " + f);
+        System.out.println("Modulus: " + mod);
+
+    }
+
+}
